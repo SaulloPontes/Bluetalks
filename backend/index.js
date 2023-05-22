c09332cf6cb16616bc3e044780c2a9aaa070ee37
lucasmp77 <87146612+lucasmp77@users.noreply.github.com>
2023-05-22 20:56:05 -0300

    Criando backend para categorias

--- backend/index.js
+++ backend/index.js
@@ -81,6 +81,56 @@ router.delete('/perfil/:id', async function(req,res,next){
 })
 
 
+router.get('/categoria/:id?',async function(req,res,next){
+  try {
+     const db = await connect();
+    if (req.params.id) {
+      res.json(await db.collection("categoria").findOne({_id: new ObjectId(req.params.id)}))
+    } else {
+      res.json(await db.collection("categoria").find().toArray());
+    }
+   } catch (ex) {
+     console.log(ex)
+    res.status(400).json({erro:`${ex}`});
+   }
+})
+
+
+router.post('/categoria',async function(req,res,next){
+  try {
+     const categoria   =   req.body;
+     const db = await connect();
+     res.json(await db.collection("categoria").insertOne(categoria))
+   } catch (ex) {
+     console.log(ex)
+    res.status(400).json({erro:`${ex}`});
+   }
+})
+
+router.put('/categoria/:id', async function(req,res,next){
+ try{
+   const categoria = req.body;
+   const db = await connect();
+    res.json(await db.collection("categoria").updateOne({_id: new ObjectId(req.params.id)},{$set:categoria}))
+ }catch(ex){
+    console.log(ex)
+    res.status(400).json({erro:`${ex}`});
+ }
+})
+
+
+
+router.delete('/categoria/:id', async function(req,res,next){
+ try{
+   const db = await connect();
+    res.json(await db.collection("categoria").deleteOne({_id: new ObjectId(req.params.id)}))
+ }catch(ex){
+    console.log(ex)
+    res.status(400).json({erro:`${ex}`});
+ }
+})
+
+
 app.use('/',router);
 app.listen(3000)
 console.log("Servidor funcionando ")
