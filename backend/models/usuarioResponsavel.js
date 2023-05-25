fa867dc9eb3987c4c039604778bc761bf534adee
Saulo araujo <102762350+SaulloPontes@users.noreply.github.com>
2023-05-25 14:54:49 -0300

    Update usuarioResponsavel.js

--- backend/models/usuarioResponsavel.js
+++ backend/models/usuarioResponsavel.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 
 const UsuarioResponsavelSchema = new mongoose.Schema({
-    usuarioResponsavel : {author: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' }},
-    usuarioDependente : {author: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' }}
+    usuarioResponsavel :  { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario'},
+    usuarioDependente :  { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario'}
   },{timestamps: true});
 
 
