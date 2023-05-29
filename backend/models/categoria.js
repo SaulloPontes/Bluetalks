2f7df20bcf2af3dcee973848b0443087648555c4
Saulo araujo <102762350+SaulloPontes@users.noreply.github.com>
2023-05-29 09:56:22 -0300

    troca type: Buffer para type: String

--- backend/models/categoria.js
+++ backend/models/categoria.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const CategoriaSchema = new mongoose.Schema({
     criador_categoria :  {type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true},
-    imagem: { type: Buffer, required: true },
+    imagem: { type: String, required: true },
     nome_categoria: { type: String, required: true },
     url_convite : {type: String, required: false }
   },{timestamps: true});
